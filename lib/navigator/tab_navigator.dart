229f4b6ff18facc9bad320c1e72c4808d3a1ffcb
houyc2 <houyc2@spdb.com.cn>
2021-04-01 00:37:19 +0800

    底部导航栏搭建

---
+++ lib/navigator/tab_navigator.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/home_page.dart';
+import 'package:flutter_app/pages/my_page.dart';
+import 'package:flutter_app/pages/search_page.dart';
+import 'package:flutter_app/pages/cart_page.dart';
+
+class TabNavigator extends StatefulWidget {
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+  final _defaultColor = Colors.grey;
+  final _activeColor = Colors.pink;
+  int _currentIndex = 0;
+  final PageController _controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _controller,
+        children: <Widget>[
+          HomePage(),
+          SearchPage(),
+          CartPage(),
+          MyPage(),
+        ],
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            _controller.jumpToPage(index);
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          items: [
+            _bottomItem('首页', Icons.home, 0),
+            _bottomItem('分类', Icons.search, 1),
+            _bottomItem('购物车', Icons.shopping_cart, 2),
+            _bottomItem('我的', Icons.account_circle, 3),
+          ]),
+    );
+  }
+
+  _bottomItem(String title, IconData icon, int index) {
+    return BottomNavigationBarItem(
+        icon: Icon(
+          icon,
+          color: _defaultColor,
+        ),
+        activeIcon: Icon(
+          icon,
+          color: _activeColor,
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+              color: _currentIndex != index ? _defaultColor : _activeColor),
+        ));
+  }
+}
+
